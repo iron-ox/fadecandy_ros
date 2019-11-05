31a43666a9e0c1eed0e65c48c9f464743d10704d
Jon Binney <jon.binney@gmail.com>
2019-11-05 11:47:26 -0800

    Add useful client functions

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['fadecandy_ros'],
-    scripts=['scripts/fadecandy_node'],
     package_dir={'': 'src'}
 )
 
