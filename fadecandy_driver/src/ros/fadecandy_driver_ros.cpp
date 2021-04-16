bc8c5c7302d21097cc7ad872282c238c17a602ae
Jad Haj Mustafa <jado-91@hotmail.com>
2021-04-16 20:18:01 +0200

    Feat/refactor to c++ (#20)
    * wip
    
    * wip
    
    * wip
    
    * fix: usb configuration and reconnectivity
    
    * feat: add diagnostics updater
    
    * clang formatted code
    
    * fix: lint + unnecessary variables + dependecies
    
    * fix: remove unnecessary additions
    
    * fix: lookup table colors
    
    * fix: consistent styling
    
    * fix: random mistakes
    
    * add exceptions with handling
    
    * fix: typos +  magic numbers + declarations
    
    * fix: erase values of vector colors instead of rewrite them
    
    * fix: license
    
    * fix: local declaration
    
    * fix: overflow exception
    
    * fix: libusb dependency
    
    * fix: remove unnecessary additions
    
    * fix: white spaces
    
    * wip
    
    * reconnect using timer callback
    
    * fix: styling
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * connect immediately at startup
    
    * some refactoring
    
    * initialize libusb once in constructor + get rid of all asserts
    
    * correct variable type to int + add a condition to release device
    
    * remove unnecessary prints
    
    * cleanup  + doc
    
    * missing license header
    
    * removed python
    
    * rospy test dep
    
    * fixup includes
    
    * fix: releaseInterface
    
    * fix: libusb-1.0-dev dep
    
    * fix: rename node to fadecandy_node
    
    * chore: Use pkgconf for finding libusb
    
    Co-authored-by: jad <jad.hajmustafa@eurogroep.com>
    Co-authored-by: Rein Appeldoorn <reinzor@gmail.com>

---
+++ fadecandy_driver/src/ros/fadecandy_driver_ros.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2021 Eurotec, Netherlands
+ * All rights reserved.
+ *
+ * Author: Jad Haj Mustafa
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <fadecandy_msgs/LEDArray.h>
+
+#include "./fadecandy_driver_ros.h"
+
+namespace fadecandy_driver
+{
+FadecandyDriverROS::FadecandyDriverROS(double restart_patience) : restart_patience_(restart_patience)
+{
+  ros::NodeHandle nh;
+
+  diagnostic_updater_.add("Info", this, &FadecandyDriverROS::diagnosticsCallback);
+  diagnostics_timer_ = nh.createTimer(ros::Duration(1.), &FadecandyDriverROS::diagnosticsTimerCallback, this);
+
+  connect_timer_ = nh.createTimer(ros::Duration(restart_patience_), &FadecandyDriverROS::connectTimerCallback, this);
+
+  led_subscriber_ = nh.subscribe<fadecandy_msgs::LEDArray>("set_leds", 1, &FadecandyDriverROS::setLedsCallback, this);
+}
+
+void FadecandyDriverROS::run()
+{
+  setupConnection();
+  ros::spin();
+}
+
+void FadecandyDriverROS::setupConnection()
+{
+  try
+  {
+    auto serial_number = driver_.connect();
+    diagnostic_updater_.setHardwareID(serial_number);
+    ROS_INFO("Fadecandy device is connected.");
+  }
+  catch (const std::exception& e)
+  {
+    ROS_WARN_ONCE("Failed to connect to device: %s; will retry every %f seconds", e.what(), restart_patience_);
+  }
+}
+
+void FadecandyDriverROS::setLedsCallback(const fadecandy_msgs::LEDArrayConstPtr& led_array_msg)
+{
+  if (!driver_.isConnected())
+  {
+    return;
+  }
+
+  std::vector<std::vector<Color>> led_array_colors;
+  for (const auto& strip : led_array_msg->strips)
+  {
+    std::vector<Color> led_strip_colors;
+    for (const auto& color : strip.colors)
+    {
+      led_strip_colors.emplace_back(static_cast<int>(color.r * 255), static_cast<int>(color.g * 255),
+                                    static_cast<int>(color.b * 255));
+    }
+    led_array_colors.push_back(led_strip_colors);
+  }
+
+  try
+  {
+    driver_.setColors(led_array_colors);
+  }
+  catch (const std::exception& e)
+  {
+    ROS_ERROR("Error occured: %s ", e.what());
+  }
+};
+
+void FadecandyDriverROS::diagnosticsCallback(diagnostic_updater::DiagnosticStatusWrapper& diagnostic_status)
+{
+  if (driver_.isConnected())
+  {
+    diagnostic_status.summary(diagnostic_msgs::DiagnosticStatus::OK, "Connected");
+  }
+  else
+  {
+    diagnostic_status.summary(diagnostic_msgs::DiagnosticStatus::ERROR, "Disconnected");
+  }
+}
+
+void FadecandyDriverROS::diagnosticsTimerCallback(const ros::TimerEvent& e)
+{
+  diagnostic_updater_.force_update();
+}
+
+void FadecandyDriverROS::connectTimerCallback(const ros::TimerEvent& e)
+{
+  if (driver_.isConnected())
+  {
+    return;
+  }
+  setupConnection();
+}
+}  // namespace fadecandy_driver
---
+++ fadecandy_driver/src/ros/fadecandy_driver_ros.h
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2021 Eurotec, Netherlands
+ * All rights reserved.
+ *
+ * Author: Jad Haj Mustafa
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <diagnostic_updater/diagnostic_updater.h>
+#include <fadecandy_msgs/LEDArray.h>
+#include <ros/publisher.h>
+#include <ros/subscriber.h>
+
+#include "../fadecandy_driver.h"
+
+namespace fadecandy_driver
+{
+class FadecandyDriverROS
+{
+public:
+  //!
+  //! \brief FadecandyDriverRos fadecandy driver ROS wrapper
+  //! \param restart_patience Restart patience
+  //!
+  FadecandyDriverROS(double restart_patience);
+
+  //!
+  //! \brief run Listen to LED messages and publishes diagnostic of the driver
+  //!
+  void run();
+
+private:
+  //!
+  //! \brief driver_ Fadecandy driver
+  //!
+  FadecandyDriver driver_;
+
+  //!
+  //! \brief setupConnection Connect the driver to the device
+  //!
+  void setupConnection();
+
+  //!
+  //! \brief led_subscriber_ LED messages subscriber
+  //!
+  ros::Subscriber led_subscriber_;
+  void setLedsCallback(const fadecandy_msgs::LEDArrayConstPtr& msg);
+
+  //!
+  //! \brief diagnosticsCallback Diagnostics callback
+  //! \param diagnostic_status Status that should be updated
+  //!
+  void diagnosticsCallback(diagnostic_updater::DiagnosticStatusWrapper& diagnostic_status);
+
+  //!
+  //! \brief timer_ Periodic timer for updating the diagnostics
+  //!
+  ros::Timer diagnostics_timer_;
+  void diagnosticsTimerCallback(const ros::TimerEvent& e);
+
+  //!
+  //! \brief connection_check_timer_ Periodic timer for checking the connection
+  //!
+  ros::Timer connect_timer_;
+  void connectTimerCallback(const ros::TimerEvent& e);
+
+  //!
+  //! \brief diagnostic_updater_ Diagnostic updater
+  //!
+  diagnostic_updater::Updater diagnostic_updater_;
+
+  //!
+  //! \brief restart_patience_ Restart patience time
+  //!
+  double restart_patience_;
+};
+}  // namespace fadecandy_driver
